8c5c4eb31ed6d800f24e1f7d5cb7b20875f9603e
Santyzz0311 <santyg0311@gmail.com>
2024-04-17 22:32:20 -0500

    Feat: creacion proyecto con vite, instalacion de tailwindcss y react routes

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Especifica el parser de ESLint para TypeScript
+  parserOptions: {
+    ecmaVersion: 2020, // Permite la sintaxis de ECMAScript moderna
+    sourceType: 'module', // Permite el uso de módulos de ES6
+    ecmaFeatures: {
+      jsx: true, // Permite la sintaxis de JSX
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Dile a eslint-plugin-react que detecte automáticamente la versión de React
+    },
+  },
+  extends: [
+    'standard', // Usa las reglas de eslint-config-standard
+    'plugin:@typescript-eslint/recommended', // Usa las recomendaciones de @typescript-eslint/eslint-plugin
+    'plugin:react/recommended', // Usa las recomendaciones de eslint-plugin-react
+  ],
+  rules: {
+    // Aquí puedes personalizar o sobrescribir reglas
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+};
