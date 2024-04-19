e5cc46002e5fe869985666e68bc5092dd66e7304
Santyzz0311 <santyg0311@gmail.com>
2024-04-19 14:00:56 -0500

    Feat: configuracion redireccionamiento si el token existe en rutas

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,11 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Login from '../Pages/Login'
+import { useContext } from 'react'
+import { AuthContext } from '../context/authContext'
+import Home from '../Pages/Home'
 
 export default function AppRoutes () {
+  const { token } = useContext(AuthContext)!
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={token ? <Navigate to="/home" replace /> : <Login />} />
+        <Route path="/home" element={!token ? <Navigate to="/login" replace /> : <Home />} />
       </Routes>
     </Router>
   )
