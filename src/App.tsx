f0c3f8ce2a9f22a8fee45f7e1ef98bd849927012
Santyzz0311 <santyg0311@gmail.com>
2024-04-19 13:55:45 -0500

    Feat: agregacion provider authContext a la app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
 import Routes from './Routes'
+import { AuthProvider } from './context/authContext'
 
 function App () {
   return (
-    <Routes />
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
   )
 }
 
