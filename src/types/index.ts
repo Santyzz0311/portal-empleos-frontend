8a445f679c9dcee84be99db568fb9767ca29474b
Santyzz0311 <santyg0311@gmail.com>
2024-04-20 05:48:48 -0500

    Feat avances creacion home

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,7 +4,7 @@ export interface User {
   id: number;
   name: string;
   email: string;
-  rol?: string;
+  roleName: string;
 }
 
 export interface AuthContextType {
@@ -16,4 +16,56 @@ export interface AuthContextType {
 
 export interface AuthProviderProps {
   children: ReactNode;
+}
+
+export interface Errors {
+  email?: string;
+  password?: string;
+  form?: string;
+}
+
+export interface ApplyModalProps {
+  idJob: number;
+  jobTitle: string;
+  jobDescription: string;
+  userName: string;
+  setModal: (value: boolean) => void;
+}
+
+export interface JobCardProps {
+  id: number;
+  title: string;
+  description: string;
+  location: string;
+  creatorUserName: string;
+  creatorEmail: string;
+  categoryName: string;
+  hasApplied: boolean;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+}
+
+export interface Job {
+  id: number;
+  title: string;
+  description: string;
+  location: string;
+}
+
+export interface NavItemProps {
+  label: string;
+  onClick: () => void;
+}
+
+export interface Applies {
+  jobId: number;
+  jobTitle: string;
+  appliedOn: string;
+  applyDescription: string;
+  jobLocation: string;
+  jobCreatorName: string;
+  jobCreatorEmail: string;
 }
